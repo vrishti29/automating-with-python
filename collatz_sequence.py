2dd7cb38c400b0c1f40143841df736a2e95570c6
Vrishti Garg <105159048+vrishti29@users.noreply.github.com>
2024-08-30 14:14:02 +0530

    Update collatz_sequence.py

--- collatz_sequence.py
+++ collatz_sequence.py
@@ -1,20 +1,43 @@
-def collatz(n):
-    if n%2 == 0:
-        a = n//2
+'''The Collatz Sequence
+Write a function named collatz() that has one parameter named number. If number is even, then collatz() should print number // 2 and return this value.
+If number is odd, then collatz() should print and return 3 * number + 1. Then write a program that lets the user type in an integer and that keeps calling collatz() 
+on that number until the function returns the value 1.(Amazingly enough, this sequence actually works for any integer—sooner or later, using this sequence, 
+you’ll arrive at 1! Even mathematicians aren’t sure why. Your program is exploring what’s called the Collatz sequence, sometimes called “the simplest 
+impossible math problem.”) Remember to convert the return value from input() to an integer with the int() function; otherwise, it will be a string value.
+*Hint:* An integer number is even if number % 2 == 0, and it’s odd if number % 2 == 1.
+
+The output of this program could look something like this:
+Enter number:
+3
+10
+5
+16
+8
+4
+2
+1
+
+Input Validation
+Add try and except statements to the previous project to detect whether the user types in a noninteger string. Normally, the int() function will raise a
+ValueError error if it is passed a noninteger string, as in int('puppy'). In the except clause, print a message to the user saying they must enter an integer'''
+
+def collatz(num):
+    if num % 2 == 0:
+        ans = num//2
     
-    elif n%2==1:
-        a = 3*n +1
+    elif num % 2==1:
+        ans = 3*num +1
     
     else:
         print("Enter a number")
 
-    print(a)
-    return a
+    print(ans)
+    return ans
     
     
-def sequence(n):
-    while n!= 1:
-        n = collatz(n)
+def sequence(num):
+    while num!= 1:
+        num = collatz(num)
 
 number = 10
 sequence(number)
